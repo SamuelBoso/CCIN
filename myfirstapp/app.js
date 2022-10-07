4b673772dbd5556727a7e5603e3e7c96cff135b1
SamuelBoso <samuel.boso@student.htldornbirn.at>
2022-10-07 16:24:40 +0200

    test

---
+++ myfirstapp/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const { lookup } = require('geoip-lite');
+const router = express.Router();
+
+const app = express();
+var os = require('os');
+
+
+router.get('/someroute', (req,res) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const data = {hostname:os.hostname(), platform:os.platform(),ip:ip} //JSON Object 
+  res.send(JSON.stringify(data)) //write JSON object to console
+  });
+  
+  app.use('/', router);
+  app.listen(8080);
