67faa0d259a0266a7d2c2255cc931346b8dea2b9
SamuelBoso <samuel.boso@student.htldornbirn.at>
2022-10-07 16:53:39 +0200

    final_version

--- myfirstapp/app.js
+++ myfirstapp/app.js
@@ -8,7 +8,7 @@ const app = express();
 var os = require('os');
 
 
-router.get('/someroute', (req,res) => {
+router.get('/info', (req,res) => {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const data = {hostname:os.hostname(), platform:os.platform(),ip:ip} //JSON Object 
   res.send(JSON.stringify(data)) //write JSON object to console
